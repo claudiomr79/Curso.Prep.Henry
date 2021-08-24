682ac0bfbc786f5b41b2fa37bdfb79a6acc93fcc
claudio R <claudio24_sn@hotmail.com>
2021-08-23 22:48:46 -0300

    homework 3 avances

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -54,7 +54,7 @@ function saludo(idioma) {
     break;
     case "ingles": return "Hello!";
     break;
-    default: return "Hola";
+    default: return "Hola!";
   }
 }
 
@@ -66,18 +66,41 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color){
+    case "blue": return "This is blue";
+    break;
+    case "red": return "This is red";
+    break;
+    case "green": return "This is green";
+    break;
+    case "orange": return "This is orange";
+    break;
+    default: return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch(numero) {
+    case 10: 
+    case 5:
+       return true;
+      break;
+    default: return false;
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 &&  numero > 20) {
+    return true;
+  }
+  else
+    return false;
 }
 
 function esEntero(numero) {
@@ -88,6 +111,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -95,6 +123,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0){
+    return "fizzbuzz";
+  }else if (numero % 5 === 0){
+    return "buzz";
+  } else if (numero % 3 === 0 ){
+    return "fizz";
+  }else{
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
